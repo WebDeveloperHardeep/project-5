87d3b394f8ae4bc15b569c2e201c8ab0706e24dc
Hardeep <mailto:deshardeep@gmail.com>
2023-09-16 23:00:25 +0530

    refactor code to imrove quality and readability

--- js/app.js
+++ js/app.js
@@ -1,96 +1,108 @@
-const questions =[
-    {
-        'que': 'Which of the following is markup language ?',
-        'a': 'Html',
-        'b': 'Css',
-        'c': 'Javascript',
-        'd': 'PHP',
-        'correct': 'a',
+const questionListArray = [
+  {
+    que: "Which of the following is markup language ?",
+    ans: ["Html", "Css", "Javascript", "PHP"],
 
-    },
-    {
-        'que': 'What yea was javascript launced ?',
-        'a': '1999',
-        'b': '1995',
-        'c': '1954',
-        'd': 'None of above',
-        'correct': 'b',
+    correct: 0,
+  },
+  {
+    que: "What year was javascript launced ?",
+    ans: ["1999", "1995", "1954", "None of above"],
 
-    },
+    correct: 1,
+  },
 
-    {
-        'que': 'what does css stands for ?',
-        'a': 'Hypertext markup language',
-        'b': 'Cascading style sheet',
-        'c': 'jason oject notation',
-        'd': 'Helicopters Terminals Motarboats Lambarginia',
-        'correct': 'b',
+  {
+    que: "what does css stands for ?",
+    ans: [
+      "Hypertext markup language",
+      "Cascading style sheet",
+      "jason oject notation",
+      "Helicopters Terminals Motarboats Lambarginia",
+    ],
+    correct: 1,
+  },
+];
 
+let questionIndex = 0;
+let totalQuestions = questionListArray.length;
+let rightAnswers = 0,
+  wrongAnswers = 0;
+const questionBoxElement = document.getElementById("quesBox");
+const answerOptionsInputElements = document.querySelectorAll(".answerOptions");
+
+const loadQuestion = () => {
+  // total question  3
+  // all question index, 0, 1, 2
+
+  if (questionIndex == totalQuestions) {
+    return endQuiz();
+  }
+
+  resetAnswerOptions();
+
+  const currenteQuestionData = questionListArray[questionIndex]; // load question at index
+  // console.log(data);
+  questionBoxElement.innerText = ` ${questionIndex + 1} ) ${
+    currenteQuestionData.que
+  }`;
+
+  // load all answers of current question
+  currenteQuestionData.ans.forEach((item, index) => {
+    answerOptionsInputElements[index].nextElementSibling.innerText = item;
+  });
+};
+
+const submitQuiz = () => {
+  const currentQuestionData = questionListArray[questionIndex];
+  const userAnswer = getUserAnswer(); // "1"   => parseInt("1") = 1
+  // currentQuestionData.correct = 1
+
+  //   console.log(userAnswer, currentQuestionData.correct);
+
+  if (parseInt(userAnswer) === currentQuestionData.correct) {
+    rightAnswers++;
+  } else {
+    wrongAnswers++;
+  }
+
+  questionIndex++; // 0 => 1
+
+  loadQuestion(); // load question at index 1
+
+  return;
+};
+
+const getUserAnswer = () => {
+  let answer;
+  const answerOptionsArray = [...answerOptionsInputElements];
+
+  answerOptionsArray.every(function (input) {
+    if (input.checked) {
+      answer = input.value;
+      return false;
+      // break the loop when a checked answer element is found
     }
-]
 
-  let index = 0;
-  let total = questions.length;
-  let rigth = 0,
-      wrong = 0;
-  const quesBox = document.getElementById("quesBox")
-  const optionInput = document.querySelectorAll(".Option")
-  const loadQuestion = () =>{
-    if(index == total){
-       return endQuiz()
-    }
-    reset();
-    const data = questions[index]
-    console.log(data);
-    quesBox.innerText = ` ${index+1} ) ${data.que}`;
-    optionInput[0].nextElementSibling.innerText = data.a;
-    optionInput[1].nextElementSibling.innerText = data.b;
-    optionInput[2].nextElementSibling.innerText = data.c;
-    optionInput[3].nextElementSibling.innerText = data.d;
+    return true; // run the next iteration of loop
+  });
 
-}
+  return answer; // 1
+};
 
-const submitQuiz = () =>{
-    const data = questions[index]
-    const ans = getAnswer()
-    console.log(ans, data.correct)
-    if(ans === data.correct){
-        rigth++
-    }else{
-        wrong++
-    }
-    index++;
-    loadQuestion();
-    return;
-}
-const getAnswer = () =>{
-    let answer;
-    optionInput.forEach(
-        (input) => {
-            if(input.checked){
-                answer = input.value;
-            
-            }
-        }
-)
-    return answer;
-}
+const resetAnswerOptions = () => {
+  answerOptionsInputElements.forEach((input) => {
+    input.checked = false;
+  });
+};
 
-const reset = ()=>{
-    optionInput.forEach(
-        (input) => {
-          input.checked = false;
-        }
-)
-}
-
-const endQuiz = ()=>{
-    document.getElementById("box").innerHTML = `
+const endQuiz = () => {
+  document.getElementById("box").innerHTML = `
     <div style="text-align:center;">
-   <h3>Thank you for Playing quiz</h3>
-    <h2> ${rigth} / ${total} are correct</h2>
-    </div?
-    `
-}
+        <h3>Thank you for Playing quiz</h3>
+        <h2> You answered ${rightAnswers} out of ${totalQuestions} questions correctly.</h2>
+        <!-- You answered 2 out of 3 questoins correctly. -->
+    </div>`;
+};
 
-loadQuestion()
+loadQuestion();
